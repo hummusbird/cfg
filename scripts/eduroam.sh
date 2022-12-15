81554f5b9d4dda70d22402254678ea7c09e3590e
hummusbird <birb@birb.cc>
2022-12-15 10:28:51 +0000

    update eduroam.sh

--- scripts/eduroam.sh
+++ scripts/eduroam.sh
@@ -9,7 +9,7 @@ wifi.ssid eduroam \
 wifi.mode infrastructure \
 wifi-sec.key-mgmt wpa-eap \
 802-1x.eap peap \
-802-1x.identity username \
+802-1x.identity username@domain \
 802-1x.phase2-auth mschapv2 \
 802-1x.password XXXXXX
 
