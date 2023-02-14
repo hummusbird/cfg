d3cc14f0f3d6d8b9bdae87b7be7e3a06aff0f467
hummusbird <birb@birb.cc>
2023-02-14 19:38:48 +0000

    i3lock-color

---
+++ scripts/lock.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+i3lock \
+--blur 15 \
+--bar-indicator \
+--bar-max-height 5 \
+--bar-color 00000000 \
+--keyhl-color f42069ff \
+\
+--clock \
+--force-clock \
+--date-color 777777ff \
+--time-color aaaaaaff \
+\
+--verif-color aaaaaaff \
+--verif-pos tx:60 \
