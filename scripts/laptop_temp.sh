1b05213fddc94d3fb3eef98db3e9702dba73170a
hummusbird <birb@birb.cc>
2023-05-17 19:29:42 +0100

    add mimeapps & others

--- scripts/laptop_temp.sh
+++ scripts/laptop_temp.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 while true
 do
-	cputemp=$(cat /sys/class/thermal/thermal_zone8/temp)
+	cputemp=$(cat /sys/class/thermal/thermal_zone6/temp)
 	expr $cputemp / 1000  > /tmp/cputemp
 	sleep 0.5s
 done
